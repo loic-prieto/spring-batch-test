6615d7eed3ce521e5cc11275b313ae5f90465f03
Loic Prieto <loic.sephiroth@gmail.com>
2017-12-05 23:49:24 +0100

    First commit for a Spring Batch application with HSQLDB database

---
+++ src/main/java/org/sephire/tests/springbatch/Application.java
@@ -0,0 +1,11 @@
+package org.sephire.tests.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
